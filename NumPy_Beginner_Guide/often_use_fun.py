1f56d1dd6a719a6f093ffb348ac7219ead1f1d16
laoxian423 <laoxian1015@163.com>
2019-05-20 12:35:13 +0800

    docs(often_use_fun.py) rate volatility date

--- NumPy_Beginner_Guide/often_use_fun.py
+++ NumPy_Beginner_Guide/often_use_fun.py
@@ -44,7 +44,7 @@ def stock_twap():
     示例都仅仅是为了说明问题。
     """
     C = np.loadtxt('NumPy_Beginner_Guide/appl.csv', delimiter=' ', usecols=(4), unpack=True)
-    # 生成一个自然数序列，做一个粗澡的权重
+    # 生成一个自然数序列，做一个粗糙的权重
     t = np.arange(len(C))
     return np.average(C, weights=t)
 
@@ -57,8 +57,122 @@ def max_min_price():
     # ptp() 返回C 中最大值和最小值的极差
     return (np.max(C),np.min(C),np.ptp(C))
 
+""" 统计分析 """
+def calc_median():
+    """ 计算中位数 、方差 
+    
+    Numpy中方差的定义和统计学中定义不一致，但更为通用。
+    方差指个体数据和个体数据算术平均值的离差平方和，除以个体个数所得到的值
+    这个值越大，表面参与计算的个体数据之间差距越大，收盘价方差表示一定的收
+    盘价的价格变动幅度。
+    """
+
+    C = np.loadtxt('NumPy_Beginner_Guide/appl.csv', 
+                    delimiter=' ', 
+                    usecols=(4), 
+                    unpack=True)
+    # 验证一下中位数是否正确
+    # sorted_close = np.msort(C)
+    # N = len(C)
+    # sorted_close = list(sorted_close)
+    # print(sorted_close[int((N-1)/2)])
+
+    return (np.median(C), np.var(C))
+
+# 中位数和方差
+# abc = calc_median()
+# print("media = {0}, VAR = {1}".format(abc[0], abc[1]))
 
 
-print(max_min_price())
+def rate_of_return():
+    """ 股票收益率 
+    
+    简单收益率：指相邻两个价格之间的变化率。
+    对数收益率：指所有价格取对数后两两之间的差值。
+    """
+    # 计算简单收益率
+    # NumPy的diff函数可以返回一个由相邻数组元素的差值构成的数组，类似于微分。
+    # 我们还需要用差值除以前一天的价格。
+    # diff返回的数组比收盘价少一个元素。
+    C = np.loadtxt('NumPy_Beginner_Guide/appl.csv', 
+                    delimiter=' ', 
+                    usecols=(4), 
+                    unpack=True)
 
+    returns = np.diff(C) / C[:-1]
+    # print(returns)
+    # 计算标准差
+    # print("Standard deviation",np.std(returns))
+
+    # 计算对数收益率
+    logreturns = np.diff(np.log(C))
+
+    return (returns,logreturns)
+
+
+# abc = rate_of_return()
+# posertindices = np.where(abc[0] > 0)    
+# print('元素中所有正值的索引:',posertindices)
+# for i in posertindices:
+#     print(abc[0][i])
+
+def calc_Volatility():
+    """ 计算波动率
+
+    历史波动率（年波动率或月波动率）：
+    年波动率等于对数收益率的标准差除以均值，再除以交易日倒数的平方根，通常取252天。
+    """
+    logreturns = rate_of_return()[1]
+    annual_volatility = np.std(logreturns)/np.mean(logreturns)
+    # Python中整数的除法和浮点数的除法运算机制不同，这里必须用浮点除法才能正确。
+    annual_volatility = annual_volatility / np.sqrt(1./252.)
+    return annual_volatility
+
+# print('收盘价年波动率:',calc_Volatility())
+
+""" 日期分析 
+    Numpy会尝试把日期转换成浮点数，需要显示的告诉Numpy怎样来转换日期。
+    在loadtxt中有一个参数 converters ,它是一个数据列和转换函数之间进行映射的字典。
+    需要自己写出转换函数。
+"""
+import datetime
+
+def date2num(s):
+    """ 日期转换成对应的数字（星期几）
+
+    monday: 0  .....  sunday: 6
+    """
+    return datetime.datetime.strptime(str(s,encoding='utf8'), "%m-%d-%Y").date().weekday()
+
+
+def trans_dates_number():
+    """ 将日期转换成一个数字序列 
+    
+    将股票日期转换成数字星期几，然后计算每个星期几的平均值
+    """
+
+    dates, close = np.loadtxt('NumPy_Beginner_Guide/appl.csv', 
+                       delimiter=' ', 
+                       usecols=(0, 4), 
+                       converters={0:date2num},  # 这里读出的是字节码，需要在转换函数中转成字符串
+                       unpack=True)
+    averages = np.zeros(5)
+    for i in range(5):
+        # 取出每星期几对应的索引
+        indices = np.where(dates == i)
+        # 根据这个索引取出对应的收盘价
+        prices = np.take(close, indices)
+        # 算术平均
+        avg = np.mean(prices)
+        averages[i] = avg 
+
+    return averages
+
+
+
+# avg = trans_dates_number()
+# print(avg,type(avg))
+# #print('星期一:{0} 星期二：{1} 星期三：{2} 星期四：{3} 星期五：{4}'.format(avg[0],avg[1],avg[2],avg[3],avg[4]))
+# # {0[0]}  第一个0 表示format中的第一个变量（list)，[0]表示这个变量的第一个元素
+# print('星期一:{0[0]} 星期二：{0[1]} 星期三：{0[2]} 星期四：{0[3]} 星期五：{0[4]}'.format(avg))
 
