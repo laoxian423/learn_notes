da2bdb99dc320017d370af4634969b11f5a0fe6d
laoxian423 <laoxian1015@163.com>
2019-06-20 16:58:07 +0800

    docs(numpynotes.py):update random,loadtxt

--- data_analysis/main_packages/numpynotes.py
+++ data_analysis/main_packages/numpynotes.py
@@ -5,11 +5,12 @@
 #       对Numpy的学习资料整理，以函数的形式进行组织和演示
 # 历史版本 ：
 #       2019-06-03 ：创建 刘昕
-#       2019-06-03 ：create_array()  access_array() common_func_and_attr() maths()
+#       2019-06-03 ：create_array()  access_array() common_func_and_attr() maths_func01()
 #       2019-06-17 : numerical_oper()  sort_function()
 #                    6月10日母亲住院做膝盖置换手术，好久没学习了。
 #       2019-06-18 : reshape_array()
 #       2019-06-19 ：修改更新了create_array()
+#       2019-06-20 ：更新了maths_func01（），新增access_file()
 
 
 
@@ -20,7 +21,7 @@ import numpy as np
 def create_array():
     """ndarray的创建
 
-    关键字：创建数组 一维 二维 多维 网格数据 线性数据 行向量 列向量 对数数据
+    关键字：创建数组 一维 二维 多维 网格数据 线性数据 行向量 列向量 对数数据 数据空间 全0 全1 单位矩阵
     """
 
     # 方式1:用python的list结构创建一维数组
@@ -81,7 +82,21 @@ def create_array():
     print(16, x)
     print(17, y)
 
-create_array() 
+    # 全零和全1的数据
+    arr1 = np.zeros(10)
+    arr2 = np.ones(10)
+    x = np.arange(1,10).reshape(3,3)
+    arr3 = np.zeros_like(x)
+    arr4 = np.ones_like(x)  
+    print(18, arr1)
+    print(19, arr2)
+    print(20, arr3)
+    print(21, arr4)
+
+    # 创建单位矩阵
+    print(22, np.identity(10))
+
+#create_array() 
 """总结：
     1、归类：
         数据结构。NumPy目前来看发现有两种数据结构ndarray和matrix,ndarray它是一个可变数组，意思是可以修改数据值。
@@ -98,7 +113,7 @@ create_array()
         由于可直接运算，如加减乘除，一个array可以乘以一个常数，也可以乘以另外一个array,做一一对应的运算。感觉用于
         矩阵运算应该很方便，不过NumPy中有专门做矩阵运算的数据结构matrix。不过ndarray是用c实现的，应该说NumPy大部分
         运算都是c实现的，从速度上讲用它就很划算。
-        本部分后半部分增加了一些生成专门数据的函数，在数据科学中比较有用。用来生产模拟数据或者相应的数据空间。
+        本部分后半部分增加了一些生成专门数据(数据空间）的函数，在数据科学中比较有用。用来生产模拟数据或者相应的数据空间。
 --------------------------------------------------------------------------------------------------"""
 
 def access_array():
@@ -185,11 +200,48 @@ def common_func_and_attr():
 def maths_func01():
     """ 数学函数
 
-    关键字：随机数
+    关键字：随机数 按位相乘 矩阵相乘 逻辑运算 随机整数 随机浮点数 随机高斯分布 随机种子 洗牌
     """
     # 生成10个1以内的随机浮点数。返回一个ndarray
     rand1 = np.random.rand(10)
     print(1, '\t', rand1, type(rand1))
+
+    # 按位相乘
+    x = np.array([[1, 2],
+                  [6, 7]])
+    y = np.array([[2, 3],
+                  [4, 5]])
+    print(2, np.multiply(x, y))
+    # 矩阵相乘
+    print(3, np.dot(x, y))
+
+    # 逻辑运算
+    x = np.array([1, 1, 0])
+    y = np.array([0, 0, 1])
+    print(4, np.logical_and(x, y))
+    print(5, np.logical_or(x, y))
+    print(6, np.logical_not(x, y))
+    print(7, np.logical_xor(x, y))
+
+    # 随机整数
+    print(8, np.random.randint(10,size=(3,2)))
+    print(9, np.random.randint(0, 10, 4))
+
+    # 随机高斯分布
+    mu, sigma = 0, 0.1
+    n = np.random.normal(mu, sigma, 10)
+    print(10, n)
+
+    # 洗牌（随机打乱数据顺序）
+    arr1 = np.arange(10)
+    print(11, arr1)
+    np.random.shuffle(arr1)
+    print(12, arr1)
+
+    # 随机种子(随机数不再每执行一次变化一次)
+    np.random.seed(1)
+    arr1 = np.random.randint(0,10,5)
+    print(13, arr1)
 
 #maths_func01()
 
@@ -199,10 +251,14 @@ def maths_func01():
         随机数是数学中很重要的部分，每种语言都提供，NumPy的random类中提供了好几个随机数的函数。python本身也提供
         但numpy似乎提供更多的，比如随机整数randint(),虽然通过处理小数也能得到随机整数，但是NumPy的大部分运算和
         函数都是 c 编译的，所以还是能用它提供的就用。
+        Numpy 对矩阵和数组的运算提供了两套函数，他们的数据结构是一样的。
     3、用途：
         随机数的用途很广，在数据分析和统计分析中都有很重要的地位。通过对随机浮点数的处理，我们可以生成很多有用的数据。
         比如在绘制曲线，模拟分布等等。
-
+        关于随机种子，说来惭愧至极，多少年了我一直不知道这玩意是干嘛的，这次才弄明白，日了狗了，以前从来没有深究过
+        这个东西，记得25年前用Basic编程时就没弄清这个种子的用途，后来确实也没有用过它，但是一直心存疑问，这
+        是个什么玩意，也没有去查过。嗯，好像也不对，当时Basic下，不设置种子的话，随机数每次不会变，Numpy下是设置了
+        种子，每次不会变。这种子到底是个什么？是数学上的定义，还是计算机上的措施？
 --------------------------------------------------------------------------------------------------"""
 
 def numerical_oper():
@@ -332,3 +388,13 @@ def reshape_array():
         函数没有涉及，估计主要使用pandas了。
 --------------------------------------------------------------------------------------------------"""
 
+def access_file():
+    """ 文件访问
+
+    关键字：文件保存  文件读取  分隔符 指定列 指定行
+    """
+    path='data_analysis/main_packages/'
+    arr1 = np.loadtxt(path+'test.txt', delimiter=',', skiprows=1)
+    print(arr1)
+
+access_file()
